ce3129a2e29f910a6d9b3f8da4f8fc0bb966989c
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-07-31 21:49:02 +0300

    change task

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const elements = {
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  spanLoader: document.querySelector('.js-loader')
+};
+
+elements.select.hidden = true;
+elements.error.hidden = true;
+elements.loader.hidden = false;
+elements.spanLoader.hidden = false;
+
+fetchBreeds()
+  .then(data => {
+    elements.select.innerHTML = createList(data);
+    new SlimSelect({
+      select: 'select',
+    });
+  })
+  .catch(error => {
+    elements.error.hidden = false;
+    Notiflix.Report.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  })
+  .finally(() => {
+    elements.select.hidden = false;
+    elements.loader.hidden = true;
+    elements.spanLoader.hidden = true;
+  });
+
+function createList(arr) {
+  return arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+}
+
+elements.select.addEventListener('change', handlerSelect);
+
+function handlerSelect(evt) {
+  const userSelect = evt.target.value;
+  elements.catInfo.hidden = true;
+  elements.loader.hidden = false;
+  elements.spanLoader.hidden = false;
+  fetchCatByBreed(userSelect)
+    .then(info => {
+      console.log(info);
+      elements.catInfo.innerHTML = createCatInfo(info);
+    })
+    .catch(error => {
+      elements.error.hidden = false;
+      Notiflix.Report.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      elements.catInfo.hidden = false;
+      elements.loader.hidden = true;
+      elements.spanLoader.hidden = true;
+    });
+}
+
+function createCatInfo(catDesc) {
+  return catDesc
+    .map(({ url, breeds }) => {
+      const { name, description, temperament } = breeds[0];
+      return `<img src="${url}" alt="${name}" width = 400>
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p><b>Temperament: </b>${temperament}</p>`;
+    })
+    .join('');
+}
